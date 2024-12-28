c1e6f5ee0f49373616a6d681d0037593991c13c4
shivaraj110 <shivarajchandaragi9@gmail.com>
2024-12-28 23:07:45 +0530

    configured for loading as a browser extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import PersonalLinks from "./pages/PersonalLinks";
 import PublicLinks from "./pages/PublicLinks";
 import Profile from "./pages/Profile";
